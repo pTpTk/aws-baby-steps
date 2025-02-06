b0bb8364157f84278795506f7019a670089b8caf
pTpTk <pTpTk@node0.ptptk-240001.rperf-pg0.utah.cloudlab.us>
2025-02-06 02:06:53 -0700

    working stage 3

---
+++ 3.deploy_single_file_go_function/test.py
@@ -0,0 +1,12 @@
+import boto3
+
+session = boto3.Session(profile_name='my-dev-profile')
+client = session.client('lambda')
+response = client.invoke(
+    FunctionName='myFunction',
+    # InvocationType='Event', # async invocation
+    InvocationType='RequestResponse', # sync invocation
+    Payload='{"key1": "vallll", "key2": "vaaaallll", "key3": "vvvvvvallll"}'
+)
+payload = response['Payload'].read()
+print(payload)
